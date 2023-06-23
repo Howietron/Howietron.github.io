97a65342cffdca7ab05eaa7b7952a0a7026eaec3
Howie <ganbeicing@foxmail.com>
2023-06-23 22:32:31 +0800

    update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -124,7 +124,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    repo: 'Howietron/Howietron.github.io', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
